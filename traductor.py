05579397c984dcbd7319ab66302fe3a604acc672
PGX <snakekillah217@gmail.com>
2024-12-29 15:21:30 -0600

    Update traductor.py

--- traductor.py
+++ traductor.py
@@ -25,4 +25,4 @@ def index():
  return render_template('index.html', contenido=contenido)
 
 if __name__ == '__main__':
- app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
+ app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5001))
